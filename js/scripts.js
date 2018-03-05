af3bf0d8be3b7fc67710473e852a59600b9c066b
Alexey <komov-1@ya.ru>
2018-03-05 18:18:00 +0700

    поработал со слайдером

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function scriptsRun () {
 
     function getButton () {
         var button = document.querySelector(".btn");
+        if (button == null) {
+            return;
+        }
 
         button.onclick = function () {
             var searchBlock = document.querySelector('.search-form');
@@ -13,8 +16,25 @@ function scriptsRun () {
         }
     };
 
-    getButton();
+    function getSlider() {
+        var elm = document.querySelector('.price-range-input');
+        var container = elm.parentNode;
+        var values = elm.getAttribute('data-values').split(' ');
 
+        values.forEach(function (value, i, values) {
+            var rangePart = elm.cloneNode();
+            rangePart.type = 'range';
+            rangePart.removeAttribute('data-values');
+            rangePart.value = value;
+            rangePart = container.insertBefore(rangePart, elm);
+        });
+
+        elm.remove();
+
+    };
+
+    getButton();
+    getSlider();
 }
 
 window.onload = scriptsRun;
